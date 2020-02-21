da2aef25d1035f99dc42a4b322bbe208e2dd2966
Peter Dol <herrpiet@hotmail.com>
2020-02-21 22:14:57 +0100

    first version

---
+++ WebJobsBuilderExtensions.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Azure.WebJobs;
+
+namespace nrdkrmp.RoutePriority
+{
+    public static class WebJobsBuilderExtensions
+    {
+        [ExcludeFromCodeCoverage]
+        public static IWebJobsBuilder AddRoutePriority(this IWebJobsBuilder builder)
+        {
+            if (builder is null)
+                throw new System.ArgumentNullException(nameof(builder));
+
+            builder.AddExtension<RoutePriorityExtensionConfigProvider>();
+
+            return builder;
+        }
+    }
+}
